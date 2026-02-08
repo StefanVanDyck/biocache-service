db76c66515349b797e4fe30a47e7abeda07991ac
Stefan Van Dyck <vandyck.stefan@gmail.com>
2026-02-08 11:58:38 +0100

    Try adding support for facet.contains

--- src/main/java/au/org/ala/biocache/dao/SearchDAOImpl.java
+++ src/main/java/au/org/ala/biocache/dao/SearchDAOImpl.java
@@ -1394,6 +1394,8 @@ public class SearchDAOImpl implements SearchDAO {
                 solrQuery.add("facet.offset", Integer.toString(searchParams.getFoffset()));
             if (StringUtils.isNotEmpty(searchParams.getFprefix()))
                 solrQuery.add("facet.prefix", searchParams.getFprefix());
+            if (StringUtils.isNotEmpty(searchParams.getFcontains()))
+                solrQuery.add("facet.contains", searchParams.getFcontains());
         }
 
         solrQuery.setRows(searchParams.getPageSize());
